3e6257d7b340d70ee1be13a10a3e3d42339ecf27
Kenneth Curtis <Kenneth.D.Curtis@outlook.com>
2016-11-11 14:02:17 -0600

    introduction to java variable types and functions

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,25 @@
  */
 public class HelloWorld {  //psvm + enter will autotype:
     public static void main(String[] args) {
+        //you have to declare the data type when you declare a variable in JAVA
+        int dayNumber = 5; // declaring an integer
+        long myAge = 21; // long is for bigger values
+
+
+        float price;
+
+
+        price = (float) 2.3; // price = (<type>) 2.3;  //type casting
+
+        double anotherPrice = 6.5; //a decimal is read by default as a double you will have to declare it as a float first
+
+
+        char vowel;
+        vowel = 'e';  //single quotes are reserved for single characters that is why you cannot use double quotes for strings
+
+        boolean isFriday = true; //declaring a boolean
+
+
         System.out.printf("Hello World!"); // JS: window.console.log('Hello World!');
         // type: sout to autotype System.out.printf("");
         System.out.printf("\nHello %s, today is %s, amd it's the %dth day of the week ",
@@ -11,5 +30,12 @@ public class HelloWorld {  //psvm + enter will autotype:
                 "Friday",
                 5
         );
+
+        int result = multiply(2, 3);
+        System.out.println("2 multiplied by 3 is " + result);
+
+    }
+    public static int multiply(int a, int b) {
+        return a *b;
     }
 }
