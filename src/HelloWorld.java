af3dbdb2835e3bc4c400cfa9a71fb51d22fa055d
Kenneth Curtis <Kenneth.D.Curtis@outlook.com>
2016-11-11 11:43:09 -0600

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * Created by KJCurtis on 11/11/16.
+ * First HelloWorld for JAVA
+ */
+public class HelloWorld {  //psvm + enter will autotype:
+    public static void main(String[] args) {
+        System.out.printf("Hello World!"); // JS: window.console.log('Hello World!');
+        // type: sout to autotype System.out.printf("");
+        System.out.printf("\nHello %s, today is %s, amd it's the %dth day of the week ",
+                "Codeup",
+                "Friday",
+                5
+        );
+    }
+}
