efb21db0a0ca87f6bfb9ead37a7c2e203a560559
Kenneth Curtis <Kenneth.D.Curtis@outlook.com>
2016-11-14 13:48:25 -0600

    moving on to pig latin exercise// pseudo text atm

--- src/ctrl_stmnts_loops_exercise.java
+++ src/ctrl_stmnts_loops_exercise.java
@@ -2,4 +2,24 @@
  * Created by KJCurtis on 11/14/16.
  */
 public class ctrl_stmnts_loops_exercise {
+    public static void main(String[] args) {
+        int[] numbersByFive = {5,10,15,20,25,30};
+        int countToFifteen = 0;
+        System.out.println("while loop print from five to fifteen \n");
+        while (countToFifteen <= 15) {
+            if (countToFifteen < 5) {
+                countToFifteen = 5;
+            }
+            System.out.println(countToFifteen);
+            countToFifteen++;
+        }
+
+
+        System.out.println("for loop through array by five \n");
+        for (int i = 0; i < numbersByFive.length; i++) {
+            System.out.println(numbersByFive[i]);
+        }
+
+
+    }
 }
---
+++ src/strings_exercise.java
@@ -0,0 +1,33 @@
+/**
+ * Created by KJCurtis on 11/14/16.
+ */
+public class strings_exercise {
+    public static void main(String[] args) {
+        String message = "We don't need no ";
+        int i = 0;
+        while (i < 2) {
+            if (i > 2) {
+                break;
+            } else if (i == 0) {
+                System.out.printf(message + "education \n");
+            } else {
+                System.out.printf(message + "thought control");
+            }
+            i++;
+        }
+        System.out.println("\n Pig latin Exercise: \n");
+
+        /*
+        Take in user input
+        prompt the user if they would like to translate the string
+        convert to lowercase
+        split the string by ' ' to an array
+        for loop through
+        if a word starts with a vowel add 'way' to the ending
+        if a word starts with a consonant move the consonants before the first vowel to the end then add 'ay' to the end
+        treat 'y' as a consonant
+
+         */
+
+    }
+}
