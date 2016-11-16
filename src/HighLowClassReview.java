398bb3b48e3f386b517774022d8832759c08fb9a
Kenneth Curtis <Kenneth.D.Curtis@outlook.com>
2016-11-16 14:42:08 -0600

    continuing mapping the functions/methods

--- src/HighLowClassReview.java
+++ src/HighLowClassReview.java
@@ -1,14 +1,31 @@
 /**
  * Created by KJCurtis on 11/16/16.
  */
+import java.util.Scanner;
+
 public class HighLowClassReview {
     public static void main(String[] args) {
         int random;
         int userGuess;
 
         random = getRandomNumber();
+        userGuess = getUserGuess();
+        if (random == userGuess) {
+            System.out.println("Good Guess!");
+        } else if (random > userGuess) {
+            System.out.println("HIGHER");
+        } else {
+            System.out.println("LOWER");
+        }
     }
     public static int getRandomNumber() {
         return (int) Math.ceil(Math.random() * 100);
+    }
+    public static int getUserGuess() {
+        int userInput;
+       Scanner sc = new Scanner(System.in);
+        System.out.print("PLease guess a number between 1 and 100: ");
+        userInput = sc.nextInt();
+        return userInput;
     }
 }
