7130f30a0c34fcdba8cf62f8e5658ebb71d81e7c
Kenneth Curtis <Kenneth.D.Curtis@outlook.com>
2016-11-16 14:37:08 -0600

    noting down the in class example

---
+++ src/HighLowClassReview.java
@@ -0,0 +1,14 @@
+/**
+ * Created by KJCurtis on 11/16/16.
+ */
+public class HighLowClassReview {
+    public static void main(String[] args) {
+        int random;
+        int userGuess;
+
+        random = getRandomNumber();
+    }
+    public static int getRandomNumber() {
+        return (int) Math.ceil(Math.random() * 100);
+    }
+}
